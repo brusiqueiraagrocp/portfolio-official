d9867333401f99b534d6ca756d2a0781f5aee36e
brusiqueiraagrocp <120668361+brusiqueiraagrocp@users.noreply.github.com>
2024-03-27 21:38:30 -0300

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import { SiGitlab } from "react-icons/si"; // Importar o ícone do GitLab
+import { SiGitlab } from "react-icons/si"; 
+import { CgWebsite } from "react-icons/cg"; // Importar o ícone do CgWebsite
 
 function ProjectCards(props) {
+  const handleClick = () => {
+    window.open(props.ghLink, "_blank"); // Abrir o link em uma nova aba ao clicar no botão GitHub ou GitLab
+  };
+
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
@@ -15,15 +19,15 @@ function ProjectCards(props) {
           {props.description}
         </Card.Text>
         {/* Renderizar o botão GitHub ou GitLab com base na propriedade 'isGitLab' */}
-        <Button variant="primary" href={props.repoLink} target="_blank">
+        <Button variant="primary" onClick={handleClick}>
           {props.isGitLab ? <SiGitlab /> : <BsGithub />} &nbsp;
-          {props.isBlog ? "Blog" : (props.isGitLab ? "GitLab" : "GitHub")}
+          {props.isGitLab ? "GitLab" : "GitHub"}
         </Button>
         {"\n"}
         {"\n"}
 
-        {/* Se o componente contiver link para demonstração e não for um blog, renderizar o botão Demo */}
-        {!props.isBlog && props.demoLink && (
+        {/* Se o componente contiver link para demonstração, renderizar o botão Demo */}
+        {props.demoLink && (
           <Button
             variant="primary"
             href={props.demoLink}
