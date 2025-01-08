bd0719ebdabcfbdfbb23a7b109489a430ba8b968
brusiqueiraagrocp <120668361+brusiqueiraagrocp@users.noreply.github.com>
2025-01-08 08:24:08 -0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,7 +7,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 // Importe o arquivo PDF corretamente
-import SoumyajitBeheraPdf from "../../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import CurriculoBruno from "../../Assets/Curriculo_Bruno.pdf";
 
 // Configure o workerSrc do pdfjs
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -26,7 +26,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={SoumyajitBeheraPdf} // Use a variável correta do PDF
+            href={CurriculoBruno} // Use a variável correta do PDF
             download="Curriculum Bruno.PDF" // Define o nome do arquivo
             style={{ maxWidth: "250px" }}
           >
@@ -36,7 +36,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={SoumyajitBeheraPdf} className="d-flex justify-content-center">
+          <Document file={CurriculoBruno} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -44,7 +44,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={SoumyajitBeheraPdf} // Use a variável correta do PDF
+            href={CurriculoBruno} // Use a variável correta do PDF
             download="Curriculum Bruno.PDF" // Define o nome do arquivo
             style={{ maxWidth: "250px" }}
           >
